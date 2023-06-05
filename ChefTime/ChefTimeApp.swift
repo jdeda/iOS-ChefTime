8b41dc2867668e35483310cbf78e9b46d7d20324
jdeda <jldeda00@gmail.com>
2023-06-05 15:49:02 -0400

    Initial commit

---
+++ ChefTime/ChefTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefTimeApp.swift
+//  ChefTime
+//
+//  Created by Jesse Deda on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ChefTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
