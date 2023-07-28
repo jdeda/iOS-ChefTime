2747f37094d2b217abeb2612c5e6a9e89998fe62
jdeda <jldeda00@gmail.com>
2023-07-28 16:47:36 -0400

    clean

--- ChefTime/SwiftUI/CustomDisclosureGroupStyle.swift
+++ ChefTime/SwiftUI/CustomDisclosureGroupStyle.swift
@@ -5,18 +5,19 @@ struct CustomDisclosureGroupStyle: DisclosureGroupStyle {
   func makeBody(configuration: Configuration) -> some View {
     HStack(alignment: .center) {
       configuration.label
-      Image(systemName: "chevron.right")
-        .rotationEffect(configuration.isExpanded ? .degrees(90) : .degrees(0))
-        .animation(.linear(duration: 0.3), value: configuration.isExpanded)
-        .font(.caption)
-        .fontWeight(.bold)
-        .frame(maxWidth : 100, maxHeight: .infinity, alignment: .trailing)
-        .buttonStyle(.plain)
-        .onTapGesture {
-          withAnimation {
-            configuration.isExpanded.toggle()
-          }
+      Button {
+        withAnimation {
+          configuration.isExpanded.toggle()
         }
+      } label: {
+        Image(systemName: "chevron.right")
+          .rotationEffect(configuration.isExpanded ? .degrees(90) : .degrees(0))
+          .animation(.linear(duration: 0.3), value: configuration.isExpanded)
+          .font(.caption)
+          .fontWeight(.bold)
+      }
+      .frame(maxWidth : 100, maxHeight: .infinity, alignment: .trailing)
+      .buttonStyle(.plain)
     }
     .contentShape(Rectangle())
     if configuration.isExpanded {
