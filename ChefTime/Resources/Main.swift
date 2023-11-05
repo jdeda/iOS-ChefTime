426eb30b4d459db9cb901e9d8802ea540a9c9f37
jdeda <jldeda00@gmail.com>
2023-11-05 18:01:22 -0500

    sytem working

--- ChefTime/Resources/Main.swift
+++ ChefTime/Resources/Main.swift
@@ -3,17 +3,31 @@ import XCTestDynamicOverlay
 
 @main
 struct ChefTimeApp: App {
-  let uuidString = "3DF50638-9B9A-48AB-87FF-9D7B943DF494"
+  let folderUUIDString = "3DF50638-9B9A-48AB-87FF-9D7B943DF494"
+  let recipeUUIDString = "3DF50638-9B9A-48AB-87FF-9D7B943DF494"
   
   var body: some Scene {
     WindowGroup {
       NavigationStack {
-        FolderView(store: .init(
-          initialState: .init(
-            folderID: .init(uuidString: uuidString)!
-          ),
-          reducer: FolderReducer.init
+        FoldersView(store: .init(
+          initialState: .init(),
+          reducer: FoldersReducer.init,
+          withDependencies: {
+            $0.database = .preview
+          }
         ))
+//        RecipeView(store: .init(
+//          initialState: .init(
+//            recipeID: .init(uuidString: recipeUUIDString)!
+//          ),
+//          reducer: RecipeReducer.init
+//        ))
+//        FolderView(store: .init(
+//          initialState: .init(
+//            folderID: .init(uuidString: folderUUIDString)!
+//          ),
+//          reducer: FolderReducer.init
+//        ))
       }
       .onAppear {
         UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).tintColor = UIColor(.yellow)
